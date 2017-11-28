6d6c6917c6aa8b1890fa8ee96636ca0c71e597f9
xiuxi <554325746@qq.com>
2017-11-28 14:52:03 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,3 +2,4 @@ yum install zip unzip net-tools
 curl https://raw.githubusercontent.com/xiuxi/socket/master/socket.zip --output socket.zip
 unzip socket.zip
 sh shadowsocksr-master/shadowsocks/run.sh
+netstat -anp | grep 8388
